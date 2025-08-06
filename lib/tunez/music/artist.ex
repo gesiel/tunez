af91e981c4d2475969b5977014536e8ab244d72e
Gesiel <gesiel@gkstech.co>
2025-08-05 21:38:59 -0300

    Add artists crud operations

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -6,6 +6,28 @@ defmodule Tunez.Music.Artist do
     repo Tunez.Repo
   end
 
+  # actions do
+  #   defaults [:create, :read, :update, :destroy]
+  #   default_accept [:name, :biography]
+  # end
+
+  actions do
+    create :create do
+      accept [:name, :biography]
+    end
+
+    read :read do
+      primary? true
+    end
+
+    update :update do
+      accept [:name, :biography]
+    end
+
+    destroy :destroy do
+    end
+  end
+
   attributes do
     uuid_primary_key :id
 
@@ -18,5 +40,4 @@ defmodule Tunez.Music.Artist do
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
-
 end
